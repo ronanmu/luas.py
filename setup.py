f3f9cd9b0936ddbdff0ec5c40caa9df344ea8f85
Ronan Murray <ronanmurray@gmail.com>
2018-05-30 23:59:29 +0100

    updated setup.py, version bump

--- setup.py
+++ setup.py
@@ -1,16 +1,21 @@
-from distutils.core import setup
+import setuptools
 
-setup(
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
     name = 'luas.py',
-    version = '0.1',
-    packages = ['luas', 'luas.models'],
+    version = '0.2',
+    packages = setuptools.find_packages(),
     url = 'https://github.com/ronanmu/luas.py',
-    download_url = 'https://github.com/ronanmu/luas.py/tarball/0.1',
+    download_url = 'https://github.com/ronanmu/luas.py/tarball/0.2',
     license = 'MIT',
     requires=['requests'],
     author = 'Ronan Murray',
     author_email = 'ronanmu@users.noreply.github.com',
     description = 'Python module for interacting with Dublin Luas real-time API',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     keywords = ['luas', 'transport', 'dublin', 'ireland', 'tram'],
     classifiers = [
         'Intended Audience :: Developers',
